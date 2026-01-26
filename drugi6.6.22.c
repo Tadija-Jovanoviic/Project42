65ca0baa26242f1d461f970e48f0c6e6e500cf70
tadija-jovanovic <jovanovic2005@gmail.com>
2026-01-26 12:40:05 +0100

    Implement matrix averaging program in C
    Added a C program structure for generating a matrix B based on the average of surrounding elements from matrix A.

--- drugi6.6.22.c
+++ drugi6.6.22.c
@@ -1,3 +1,15 @@
+/* na programskom jeziku C napisati strukturni
+program za unos elemenata celobrojne kvadratne matrice ANxN. Korisnik na početku programa unosi
+parametar K, dimenziju matrice N, a zatim i elemente matrice ANxN. Nakon unosa elemenata potrebno je
+generisati novu matricu realnih brojeva BNxN. Nova matrica se formira na osnovu elemenata matrice A, tako
+što jedan elemenat matrice B predstavlja prosečnu vrednost K elemenata oko odgovarajućeg elementa iz
+matrice A, uključujući i taj element. Prilikom izračunavanja prosečne vrednosti računati samo one elemente
+koji postoje oko tekućeg elementa. Prikazati matricu B nakon generisanja.
+Primer za K = 1 i N = 4. */
+
+
+
+
 // ne znam sta se tacno trazi u zadatku, ali 4 ugnjezdene petlje mi deluju kao overkill 
 // mozda je zadatak stvarno zajeban, pa je neophodno
 
