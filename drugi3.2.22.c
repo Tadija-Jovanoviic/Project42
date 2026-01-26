59e8cb9a1f75cc5fec4818a6460c03d1ac40489d
Ivica Rangelov <ivica.rangelov@ingsoftware.com>
2026-01-26 09:14:49 +0100

    Update drugi3.2.22.c
    rad sa fajlovima - komentar
    De Morganova pravila

--- drugi3.2.22.c
+++ drugi3.2.22.c
@@ -1,9 +1,21 @@
+// Ovaj primer odlicno ilustruje rad sa fajlovima
+// potrudi se da ga razumes i upamtis
+
 #include <stdio.h>
 int main()
 {
 	int prvi, drugi, rezNiz[100], j, i = 0;
+	
+	// otvaranje fajla za citanje - "r" - bitno
 	FILE* fprvi = fopen("prvi.txt", "r");
+	
 	FILE* fdrugi = fopen("drugi.txt", "r");
+	
+	// Ovaj uslov je jako interesantan - petlja se vrti dok se ne procitaju svi elementi iz oba fajla
+	// imas eof (feof) - end of file funkciju koja proverava da li si stigao do kraja fajla
+	// uslov moze da se zapise i kao
+	// while (!(feof(fprvi) && feof(fdrugi))) - sto je, mozda, intuitivnije, jer onda pitas - dok nije kraj i prvog i drugog
+	// ovo je direktna primena De Morganovog pravila: !(x1 and x2) = !x1 or !x2 - koje smo radili u racunarima
 	while (!feof(fprvi) || !feof(fdrugi))
 	{
 		if (feof(fprvi))
